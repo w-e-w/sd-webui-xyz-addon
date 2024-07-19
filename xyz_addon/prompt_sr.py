e098b6323bebf2ab6d3781a9431958a6e7c45c2e
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-07-20 04:37:28 +0900

    grid infotext with placeholder

--- xyz_addon/prompt_sr.py
+++ xyz_addon/prompt_sr.py
@@ -1,11 +1,13 @@
 from .utils import TrueTrue, no_type_cast, csv_string_to_list_strip
+from modules import patches, script_callbacks
+from functools import wraps
 import itertools
 
 
 def prepare_sr(vals, include_first=False):
     valslist = csv_string_to_list_strip(vals)
     placeholder = valslist[0]
-    return list(map(lambda v: (placeholder, v), valslist[0 if include_first else 1:]))
+    return list(map(lambda v: (placeholder, v[1], v[0]), enumerate(valslist[0 if include_first else 1:])))
 
 
 def combinatorics(vals, function, include_first=False):
@@ -21,10 +23,25 @@ def combinatorics(vals, function, include_first=False):
             r1, r2 = int(length[0]), int(length[1])
     r1, r2 = (r2, r1) if r1 > r2 else (r1, r2)
     r1, r2 = 0 if r1 < 0 else r1, len(replacement_list) if r2 > len(replacement_list) else r2
-    return [item for r in range(r1, r2 + 1) for item in map(lambda c: (placeholder, ', '.join(c)), function(replacement_list, r))]
+    return [item for r in enumerate(range(r1, r2 + 1)) for item in map(lambda c: (placeholder, ', '.join(c[1]), (c[0], r[0])), enumerate(function(replacement_list, r[1])))]
+
+
+def wrap_process_images(func):
+    @wraps(func)
+    def wrapper(p, *args, **kwargs):
+        res = func(p, *args, **kwargs)
+        if (prompt := getattr(p, 'xyz_addon_placeholder_original_prompt', None)) and p.all_prompts:
+            p.all_prompts[0] = prompt
+        if (negative_prompt := getattr(p, 'xyz_addon_placeholder_original_negative_prompt', None)) and p.all_negative_prompts:
+            p.all_negative_prompts[0] = negative_prompt
+        return res
+    return wrapper
 
 
 def create_axes(xyz_grid):
+    patches.patch(__name__, xyz_grid, 'process_images', wrap_process_images(xyz_grid.process_images))
+    script_callbacks.on_script_unloaded(lambda: patches.undo(__name__, xyz_grid, 'process_images'))  # in this case, undo should not be necessary
+
     class PromptSR(xyz_grid.AxisOption):
         def __init__(self, label, include_first, mode=None):
             self.label = label
@@ -38,7 +55,15 @@ def create_axes(xyz_grid):
 
         def apply(self, p, x, xs):
             """Similar functionality to the built-in Prompt S/R, but the first item will be replaced with <blank>"""
-            placeholder, replacement = x
+            placeholder, replacement, index = x
+            if not self.include_first and index == 0:
+                if getattr(p, 'xyz_addon_placeholder_original_prompt', None) is None:
+                    p.xyz_addon_placeholder_original_prompt = p.prompt
+                if getattr(p, 'xyz_addon_placeholder_original_negative_prompt', None) is None:
+                    p.xyz_addon_placeholder_original_negative_prompt = p.negative_prompt
+
+            p.xyz_addon_placeholder = not self.include_first
+
             if placeholder not in p.prompt and placeholder not in p.negative_prompt:
                 raise RuntimeError(f'{self.label}: {placeholder}" was not found in prompt or negative prompt.')
             p.prompt, p.negative_prompt = p.prompt.replace(placeholder, replacement), p.negative_prompt.replace(placeholder, replacement)
